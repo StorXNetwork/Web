8f7995aa5cbce5c9e7cd5404b04a428196b2a3f1
aishwary33 <aishwary@xinfin.org>
2021-05-10 22:14:51 +0530

    theme unsolved

--- src/components/xcloud/FileCommander.js
+++ src/components/xcloud/FileCommander.js
@@ -37,7 +37,7 @@ class FileCommander extends React.Component {
       namePath: this.props.namePath,
       selectedSortType: SORT_TYPES.DATE_ADDED,
       dragDropStyle: "",
-      // theme: localStorage.getItem("theme") == "" ? false : true,
+      theme: localStorage.getItem("theme") == "" ? false : true,
       treeSize: 0,
       isTeam: this.props.isTeam,
       dropdown: false,
@@ -377,20 +377,20 @@ class FileCommander extends React.Component {
   };
 
 
-  // changeTheme() {
-  //   this.setState({ theme: !this.state.theme });
-  //   console.log('*************', this.state.theme);
-  //   if (this.state.theme == true) {
-  //      localStorage.setItem('theme', true);
-  //   }
-  //   if (this.state.theme == false ) {
-  //      localStorage.setItem('theme', false);
-  //   }
-  //   if (localStorage.getItem('theme') == true) {
-  //     $('body').toggleClass('dark');
-  //   }
-  //   else $('body').toggleClass('dark');
-  // }
+  changeTheme() {
+    this.setState({ theme: !this.state.theme });
+    console.log('*************', this.state.theme);
+    if (this.state.theme == true) {
+      localStorage.setItem('theme', true);
+    }
+    if (this.state.theme == false) {
+      localStorage.setItem('theme', false);
+    }
+    if (localStorage.getItem('theme') == true) {
+      $('body').toggleClass('dark');
+    }
+    else $('body').toggleClass('dark');
+  }
 
   render() {
 
@@ -446,8 +446,8 @@ class FileCommander extends React.Component {
               <div className="d-flex align-items-center">
                 <div
                   className="change-mode"
-                  // onClick={() => this.changeTheme()}
-                  onChange={() => $("body").toggleClass("dark")}
+                  onClick={() => this.changeTheme()}
+                // onChange={() => $("body").toggleClass("dark")}
                 >
                   <div className="custom-control custom-switch custom-switch-icon custom-control-inline">
                     <div className="custom-switch-inner">
