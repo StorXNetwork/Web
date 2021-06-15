83aa7d2fdd7a1594f69d08844ef6eef19c9012c2
aishwary33 <aishwary@xinfin.org>
2021-06-15 16:18:35 +0530

    Dollar in SRX

--- src/components/Referred.js
+++ src/components/Referred.js
@@ -42,7 +42,7 @@ class Referred extends React.Component {
     dropdown: false,
   };
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = { value: "" };
 
@@ -576,7 +576,7 @@ class Referred extends React.Component {
                                   className="twitter"
                                   href={`https://twitter.com/intent/tweet?url=https://storx.io/new?ref=${user.uuid
                                     }&${this.parseUrl({
-                                      text: "I switched to @StorXNetwork a High Performance Truly Decentralized Cloud Storage Network. Sign up using link and get  FREE 2 GB Account + 10 SRX Tokens.\n#SRX #StorXNetwork\n\n",
+                                      text: "I switched to @StorXNetwork a High Performance Truly Decentralized Cloud Storage Network. Sign up using link and get  FREE 2 GB Account + 10 $SRX Tokens.\n#StorX #SRX #StorXNetwork\n\n",
                                     })}`}
                                   target="_blank"
                                 >
@@ -586,8 +586,7 @@ class Referred extends React.Component {
                                   className="facebook"
                                   href={`https://www.facebook.com/sharer/sharer.php?u=https://storx.io/new?ref=${user.uuid
                                     }&amp;src=sdkpreparse&${this.parseUrl({
-                                      quote:
-                                        "I switched to @StorXNetwork a High Performance Truly Decentralized Cloud Storage Network. Sign up using link and get  FREE 2 GB Account + 10 SRX Tokens.\n#SRX #StorXNetwork\n\n",
+                                      quote: "I switched to @StorXNetwork a High Performance Truly Decentralized Cloud Storage Network. Sign up using link and get  FREE 2 GB Account + 10 $SRX Tokens.\n#StorX #SRX #StorXNetwork\n\n",
                                     })}`}
                                   target="_blank"
                                   data-href={`https://storx.io/new?ref=${user.uuid}`}
@@ -598,7 +597,7 @@ class Referred extends React.Component {
                                   className="telegram"
                                   href={`https://t.me/share/url?${this.parseUrl(
                                     {
-                                      text: "I switched to @StorXNetwork a High Performance Truly Decentralized Cloud Storage Network. Sign up using link and get  FREE 2 GB Account + 10 SRX Tokens.\n#SRX #StorXNetwork\n\n",
+                                      text: "I switched to @StorXNetwork a High Performance Truly Decentralized Cloud Storage Network. Sign up using link and get  FREE 2 GB Account + 10 $SRX Tokens.\n#StorX #SRX #StorXNetwork\n\n",
                                     }
                                   )}&url=https://storx.io/new?ref=${user.uuid}`}
                                   target="_blank"
